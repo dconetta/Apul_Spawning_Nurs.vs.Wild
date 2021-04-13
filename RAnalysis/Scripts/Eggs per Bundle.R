87035b881a86e278bfd400fc090816d594baae70
Dennis Conetta <dconetta1@my.uri.edu>
2021-04-13 11:37:09 -0400

    working with 2020 Data

--- RAnalysis/Scripts/Eggs per Bundle.R
+++ RAnalysis/Scripts/Eggs per Bundle.R
@@ -5,7 +5,31 @@ rm(list=ls()) #clears workspace
 library(tidyverse)
 
 # load data 
-eggs.per.bundle <-read.csv('Data/Eggs.per.bundle.counts.csv', header=T, sep=",")
+#2019____________________________________________________
+
+eggs.per.bundle <-read.csv('Data/Oct_2019/Eggs.per.bundle.counts.csv', header=T, sep=",")
+
+eggs.per.bundle <- eggs.per.bundle %>%
+  group_by(Sample_ID, Origin ) %>%
+  summarise(Mean.eggs = mean(Num.Eggs))
+
+pdf("Output/eggs.per.bundle.pdf")
+eggs.per.bundle %>%
+  ggplot(aes(x = Origin, y = Mean.eggs, color = Origin)) +
+  labs(x = "", y = "Eggs per Bundle") +
+  geom_jitter(width = 0.1) +                                            # Plot all points
+  stat_summary(fun.data = mean_cl_normal, fun.args = list(mult = 1),    # Plot standard error
+               geom = "errorbar", color = "black", width = 0.1) +
+  stat_summary(fun = mean, geom = "point", color = "black") +          # Plot mean
+  theme_classic()
+dev.off()
+
+#stats
+t.test(Mean.eggs~Origin, data = eggs.per.bundle) #Statistically significant if p-value <0.05
+
+#2020______________________________________________________
+
+eggs.per.bundle2 <-read.csv('Data/Oct_2020/Eggs.per.bundle_2020.csv', header=T, sep=",")
 
 eggs.per.bundle <- eggs.per.bundle %>%
   group_by(Sample_ID, Origin ) %>%
